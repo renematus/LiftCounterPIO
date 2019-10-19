a75aec96c1b336e968836ed9c2ba271561787a0e
Electro <electro@rmatu.eu>
2019-10-19 20:25:13 +0200

    Add SerialCommand for setting time

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "WISOL.h"
 #include <BME280.h>
 #include <RTClock.h>
+#include <SerialCommand.h>
 #include "BendCounter.h"
 
 extern "C"
@@ -14,15 +15,23 @@ extern "C"
   void vProcessDataTask(void *pvParameters);
   void vGetDownlinkMessageTask(void *pvParameters);
   void vCheckSignalsTask(void *pvParameters);
+  void vSerialCommandReadTask(void *pvParameters);
 }
 
 void GetDeviceID();
 void Send_Pload(uint8_t *sendData, const uint8_t len);
 void getDLMsg();
+void configureTimeMinute();
+void configureTimeSec();
+void configureTimeHour();
+void configureTimeDay();
+void configureTimeMonth();
+void configureTimeYear();
 
 RTClock rt (RTCSEL_LSE);
 Isigfox *isigfox = new WISOL();
 BME280 bme(Wire,0x76);
+SerialCommand sCmd;
 
 BendCounter counter;
 
@@ -54,6 +63,14 @@ void setup() {
   counter.init();
 
 
+  //Configure serial commands
+  sCmd.addCommand("tm", configureTimeMinute);
+  sCmd.addCommand("ts", configureTimeSec);
+  sCmd.addCommand("th", configureTimeHour);
+  sCmd.addCommand("td", configureTimeDay);
+  sCmd.addCommand("tM", configureTimeMonth);
+  sCmd.addCommand("ty", configureTimeYear);
+
  xTaskCreate(vProcessDataTask,
                 (const signed char * const)"Task3",
                 configMINIMAL_STACK_SIZE + 500,
@@ -82,6 +99,13 @@ xTaskCreate(vGetDownlinkMessageTask,
                 tskIDLE_PRIORITY + 3,
                 NULL);
 
+xTaskCreate(vSerialCommandReadTask,
+                (const signed char * const)"Task5",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                tskIDLE_PRIORITY + 3,
+                NULL);
+
  // start scheduler
   vTaskStartScheduler();
   Serial1.println("Insufficient RAM");
@@ -91,13 +115,15 @@ xTaskCreate(vGetDownlinkMessageTask,
 
 void loop() {
   // // put your main code here, to run repeatedly:
-  // digitalWrite(PC13, HIGH);
-  // Serial1.println("Text");
-  // delay(1000);
-  // digitalWrite(PC13, LOW);
-  // delay(1000);
+  //Not for Free RTOS
 }
 
+void vSerialCommandReadTask(void *pvParameters) {
+    for (;;) {
+        sCmd.readSerial();
+        vTaskDelay(100);
+    }
+}
 
 void vReadBme280SensorTask(void *pvParameters) {
     for (;;) {
@@ -206,9 +232,9 @@ void Send_Pload(uint8_t *sendData, const uint8_t len){
   RecvMsg = (recvMsg *)malloc(sizeof(recvMsg));
   isigfox->sendPayload(sendData, len, 0, RecvMsg);
   for (int i = 0; i < RecvMsg->len; i++) {
-    Serial.print(RecvMsg->inData[i]);
+    Serial1.print(RecvMsg->inData[i]);
   }
-  Serial.println("");
+  Serial1.println("");
   free(RecvMsg);
 
 
@@ -230,4 +256,112 @@ void Send_Pload(uint8_t *sendData, const uint8_t len){
   Isigfox->sendPayload(sendData, len, 1);
   timer.setTimeout(46000, getDLMsg);
   */
+}
+
+//Argument vlozime  command [mezera] argument [enter]
+void configureTimeMinute()
+{
+  char * arg;
+  int minute;
+  arg = sCmd.next();
+  if (arg != NULL) {
+    minute = atoi(arg);    
+
+    tm_t set_time;
+    rt.getTime(set_time);
+    set_time.minute = minute;
+    rt.setTime(set_time);
+    Serial1.print("Minute was set to: ");
+    Serial1.println(minute);
+  }
+}
+
+//Argument vlozime  command [mezera] argument [enter]
+void configureTimeSec()
+{
+  char * arg;
+  int second;
+  arg = sCmd.next();
+  if (arg != NULL) {
+    second = atoi(arg);    
+
+    tm_t set_time;
+    rt.getTime(set_time);
+    set_time.second = second;
+    rt.setTime(set_time);
+    Serial1.print("Sec was set to: ");
+    Serial1.println(second);
+  }
+}
+
+//Argument vlozime  command [mezera] argument [enter]
+void configureTimeHour()
+{
+  char * arg;
+  int hour;
+  arg = sCmd.next();
+  if (arg != NULL) {
+    hour = atoi(arg);    
+
+    tm_t set_time;
+    rt.getTime(set_time);
+    set_time.hour = hour;
+    rt.setTime(set_time);
+    Serial1.print("Hour was set to: ");
+    Serial1.println(hour);
+  }
+}
+
+//Argument vlozime  command [mezera] argument [enter]
+void configureTimeDay()
+{
+  char * arg;
+  int day;
+  arg = sCmd.next();
+  if (arg != NULL) {
+    day = atoi(arg);    
+
+    tm_t set_time;
+    rt.getTime(set_time);
+    set_time.day = day;
+    rt.setTime(set_time);
+    Serial1.print("Day was set to: ");
+    Serial1.println(day);
+  }
+}
+
+//Argument vlozime  command [mezera] argument [enter]
+void configureTimeMonth()
+{
+  char * arg;
+  int month;
+  arg = sCmd.next();
+  if (arg != NULL) {
+    month = atoi(arg);    
+
+    tm_t set_time;
+    rt.getTime(set_time);
+    set_time.month = month;
+    rt.setTime(set_time);
+    Serial1.print("Month was set to: ");
+    Serial1.println(month);
+  }
+}
+
+//Argument vlozime  command [mezera] argument [enter]
+void configureTimeYear()
+{
+  char * arg;
+  int year;
+  arg = sCmd.next();
+  if (arg != NULL) {
+    year = atoi(arg);    
+
+    tm_t set_time;
+    rt.getTime(set_time);
+    set_time.year = year;
+    rt.setTime(set_time);
+    Serial1.print("Year was set to: ");
+    Serial1.println(year);
+  }
 }
